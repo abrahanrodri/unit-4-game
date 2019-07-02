a3b1bcb8d6183a61116506310e0e70c6807a9308
Abe <abrahan.rodri@gmail.com>
2019-07-01 22:41:06 -0400

    added random number function for computer to make a random choice in crystals'

---
+++ assets/javascript/game.js
@@ -0,0 +1,55 @@
+$(document).ready(function() {
+
+//set up variables for the counters
+var wins = 0;
+var loses = 0;
+var crystalsRetrieved = 0;
+
+//set up function for random numbers from 19 to 120
+//https://www.w3schools.com/js/js_random.asp
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  }
+
+
+
+
+$("#crystalBlue").on("click", function(){
+    
+
+    
+
+  });
+
+$("#crystalRed").on("click", function(){
+    
+    
+    
+
+});
+
+$("#crystalWhite").on("click", function(){
+    
+    
+    
+
+});
+
+$("#crystalYellow").on("click", function(){
+    
+    
+    
+
+});
+
+
+
+
+
+
+
+
+
+
+
+})
