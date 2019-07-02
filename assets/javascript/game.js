9ecd4eceb0804a378656894211c1675b54d5b70f
Abe <abrahan.rodri@gmail.com>
2019-07-02 08:46:13 -0400

    figured out how to get random values for each crystal. Also figured out how to get a random crystal number for computer.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,41 +4,41 @@ $(document).ready(function() {
 var wins = 0;
 var loses = 0;
 var crystalsRetrieved = 0;
+var crystalBlue = computerParameters(1,12);
+var crystalRed = computerParameters(1,12);
+var crystalWhite = computerParameters(1,2);
+var crystalYellow = computerParameters(1,12);
+var computerChoice = computerParameters(19,120);
 
 //set up function for random numbers from 19 to 120
 //https://www.w3schools.com/js/js_random.asp
-function getRndInteger(min, max) {
+function computerParameters(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
   }
+console.log(computerChoice);
 
 
 
-
+//set up click functions for each crystal. Similiar formula for each.
 $("#crystalBlue").on("click", function(){
-    
-
-    
+    crystalsRetrieved += crystalBlue;
+    $("#crystalsRetrieved").text(crystalsRetrieved);   
 
   });
 
 $("#crystalRed").on("click", function(){
-    
-    
-    
-
+    crystalsRetrieved += crystalRed;
+    $("#crystalsRetrieved").text(crystalsRetrieved); 
 });
 
 $("#crystalWhite").on("click", function(){
-    
-    
-    
-
+    crystalsRetrieved += crystalWhite;
+    $("#crystalsRetrieved").text(crystalsRetrieved);
 });
 
 $("#crystalYellow").on("click", function(){
-    
-    
-    
+    crystalsRetrieved += crystalYellow;
+    $("#crystalsRetrieved").text(crystalsRetrieved);    
 
 });
 
